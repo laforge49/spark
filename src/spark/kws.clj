e6af9a4df25419cb353b846ad3232dd74c8de8d7
Bill La Forge <laforge49@gmail.com>
2022-07-29 22:53:15 -0400

    Update kws.clj

--- src/spark/kws.clj
+++ src/spark/kws.clj
@@ -3,4 +3,16 @@
 (def env-gems-kw
   :spark/env-gems)
 
-(println env-gems-kw)
+(defn env-gem-kws
+  [gem-kw]
+  [env-gems-kw gem-kw])
+
+(def gem-roles-kw
+  :spark/gem-roles)
+
+(defn gem-roles-kws
+  [role-kw]
+  [gem-roles-kw role-kw])
+
+(println (env-gem-kws :fudge))
+(println (gem-roles-kws :parse))
