b67dd1f59166d314b4d5ec7011af7cba0f56c223
Bill La Forge <laforge49@gmail.com>
2022-07-31 17:48:57 -0400

    eval

--- src/spark/eval.clj
+++ src/spark/eval.clj
@@ -1,26 +1,26 @@
 (ns spark.eval
   (:require [spark.kws :as kws]))
 
-  (defn gem-eval
-    [env]
-    (let [gem-kw
-          (:param/gem env)
-          role-kw
-          (:param/role env)
-          request-kw
-          (:param/request env)
-          gem
-          @(kws/env-gem-atom env gem-kw)
-          role
-          (get-in gem (kws/gem-role-kws role-kw))
-          request
-          (request-kw role)
-          function-name
-          (:eval/function-name request)
-          function-symbol
-          (symbol function-name)
-          function
-          (resolve function-symbol)
-          ]
-      (function (into env request))
-      ))
+(defn gem-eval
+  [env]
+  (let [gem-kw
+        (:param/gem env)
+        role-kw
+        (:param/role env)
+        request-kw
+        (:param/request env)
+        gem
+        @(kws/env-gem-atom env gem-kw)
+        role
+        (get-in gem (kws/gem-role-kws role-kw))
+        request
+        (request-kw role)
+        function-name
+        (:eval/function-name request)
+        function-symbol
+        (symbol function-name)
+        function
+        (resolve function-symbol)
+        ]
+    (function (into env request))
+    ))
--- src/spark/parse.clj
+++ src/spark/parse.clj
@@ -5,14 +5,29 @@
 
 (defn select-first-matching-grammar
   [env]
-  )
+  (let [input (:param/input env)
+        parse-role (:param/parse-role env)
+        grammars (:parse/grammars parse-role)
+        ]
+    (reduce
+      (fn [result grammar]
+        (if (some? result)
+          result
+          (let [function-name (:eval/function-name grammar)
+                function-symbol (symbol function-name)
+                function (resolve function-symbol)]
+            (function (into env {:param/input input
+                                 :param/grammar grammar})))))
+      nil
+      grammars)
+    ))
 
 (defn select-equal-value
   [env]
   (let [input (:param/input env)
-        grammer (:param/grammer env)
-        value (:parse/value grammer)]
+        grammar (:param/grammar env)
+        value (:parse/value grammar)]
     (if (= input value)
-      grammer
+      grammar
       nil)
     ))
