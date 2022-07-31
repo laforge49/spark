0ab21158b05482c8e1d3637ec1ed4154e7af200a
Bill La Forge <laforge49@gmail.com>
2022-07-31 16:58:15 -0400

    boot

--- src/spark/boot.clj
+++ src/spark/boot.clj
@@ -2,22 +2,22 @@
 
 (defn add-gems-atom
   [env]
-  (let [fudge-atom (atom {:gem/name  "fudge"
-                          :gem/roles {:roles/test
-                                      {:debug/ribbit-request
-                                       {:eval/function-name
-                                        "spark.debug/ribbit"}}
-                                      :roles/parse
-                                      {:parse/select-grammars-request
-                                       {:eval/function-name
-                                        "spark.parse/select-first-matching-grammar"
-                                        :parse/grammars
-                                        [{:parse/value
-                                          :gem/roles
-                                          :eval/function-name
-                                          "spark.parse/select-equal-value"}]}}}})
+  (let [fudge {:gem/name  "fudge"
+               :gem/roles {:roles/test
+                           {:debug/ribbit-request
+                            {:eval/function-name
+                             "spark.debug/ribbit"}}
+                           :roles/parse
+                           {:parse/select-grammars-request
+                            {:eval/function-name
+                             "spark.parse/select-first-matching-grammar"
+                             :parse/grammars
+                             [{:parse/value
+                               :gem/roles
+                               :eval/function-name
+                               "spark.parse/select-equal-value"}]}}}}
         gems-atom
         (atom {:fudge
-               fudge-atom})
+               (atom fudge)})
         ]
     (into env {:env/gems-atom-kw gems-atom})))
