070600c88f8254602732afafdb1e97c943a8bcc6
Bill La Forge <laforge49@gmail.com>
2022-08-01 06:42:37 -0400

    reformat

--- src/spark/boot.clj
+++ src/spark/boot.clj
@@ -2,19 +2,19 @@
 
 (defn add-gems-atom
   [env]
-  (let [fudge {:facet/kw  :fudge
+  (let [fudge {:facet/kw    :fudge
                :facet/roles {:roles/test
-                           {:debug/ribbit-request
-                            {:eval/function-name
-                             "spark.debug/ribbit"}}
-                           :roles/parse
-                           {:parse/select-grammars-request
-                            {:eval/function-name "spark.parse/select-grammar"
-                             :parse/grammars
-                             [{:parse/value
-                               :facet/roles
-                               :eval/function-name
-                               "spark.parse/select-equal-value"}]}}}}
+                             {:debug/ribbit-request
+                              {:eval/function-name
+                               "spark.debug/ribbit"}}
+                             :roles/parse
+                             {:parse/select-grammars-request
+                              {:eval/function-name "spark.parse/select-grammar"
+                               :parse/grammars
+                               [{:parse/value
+                                 :facet/roles
+                                 :eval/function-name
+                                 "spark.parse/select-equal-value"}]}}}}
         gems-atom (atom {:fudge (atom fudge)})
         ]
     (into env {:env/gems-atom-kw gems-atom})))
