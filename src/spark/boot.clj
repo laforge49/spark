541f75f206a9307aae427645b3952def8e3da91c
Bill La Forge <laforge49@gmail.com>
2022-08-02 05:00:53 -0400

    grammars

--- src/spark/boot.clj
+++ src/spark/boot.clj
@@ -17,8 +17,10 @@
                        :roles/parse
                        {:parse/select-grammars-request
                         {:eval/function-name "spark.parse/select-grammar"
-                         :parse/grammars     {:parse/value        :facet/roles
-                                              :eval/function-name "spark.parse/select-equal-value"}}}
+                         :parse/grammars     [{:parse/value        :facet/kw
+                                               :eval/function-name "spark.parse/select-equal-value"}
+                                              {:parse/value        :facet/roles
+                                               :eval/function-name "spark.parse/select-equal-value"}]}}
                        }}
         fudge (atom fudge-value)
         gems-atom (:env/gems-atom-kw env)
