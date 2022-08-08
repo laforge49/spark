77dab80ea93e87d0eceadcc5b056af3ea4000536
Bill La Forge <laforge49@gmail.com>
2022-08-07 21:34:46 -0400

    create-gem returns gem

--- src/spark/boot.clj
+++ src/spark/boot.clj
@@ -17,30 +17,34 @@
         gems-atom (:env/gems-atom-kw env)
         ]
     (swap! gems-atom assoc gem-kw gem)
-    env))
+    gem))
 
 (defn create-role
   [env]
+  (let [role-kw (:param/role-kw env)
+        requests (:param/requests env)
+        gem (:param/gem env)
+        role-kws (kws/gem-role-kws role-kw)]
+    (swap! gem assoc-in role-kws requests)))
+
+(defn create-request
+  [env]
   (let [gem-kw (:param/gem-kw env)
         role-kw (:param/role-kw env)
-        requests (:param/requests env)
-        gem (kws/env-gem env gem-kw)
-        role-kws (kws/gem-role-kws role-kw)]
-    (swap! gem assoc-in role-kws requests)
-    ))
+        request-kw (:param/request-kw env)]))
 
 (defn create-gems
   [env]
-  (let [env (into env {:param/gem-kw :gem/fudge})]
-    (create-gem env)
-    (create-role (into env {:param/role-kw  :roles/test
+  (let [gem (create-gem (into env {:param/gem-kw :gem/fudge}))]
+    (create-role (into env {:param/gem      gem
+                            :param/role-kw  :roles/test
                             :param/requests {:debug/ribbit-request
                                              {:eval/function-name "spark.debug/ribbit"}}}))
-    (create-role (into env {:param/role-kw  :roles/parse
+    (create-role (into env {:param/gem      gem
+                            :param/role-kw  :roles/parse
                             :param/requests {:parse/select-grammars-request
                                              {:eval/function-name "spark.parse/select-grammar"
                                               :parse/grammars     [{:parse/value        :facet/kw
                                                                     :eval/function-name "spark.parse/select-equal-value"}
                                                                    {:parse/value        :facet/roles
-                                                                    :eval/function-name "spark.parse/select-equal-value"}]}}}))
-    ))
+                                                                    :eval/function-name "spark.parse/select-equal-value"}]}}}))))
