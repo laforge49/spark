02152ad44535fd2ada112d5bf6c72bf8617f0a09
Bill La Forge <laforge49@gmail.com>
2022-08-17 10:30:54 -0400

    pretty map

--- src/spark/pretty.clj
+++ src/spark/pretty.clj
@@ -1,14 +1,18 @@
 (ns spark.pretty
   (:require [clojure.string :as string]))
 
+(defn blanks
+  [count]
+  (string/join (repeat count " ")))
+
 (defn asString-
   [env]
   (let [value (:param/value env)
         initial-indent (get env :param/initial-indent 0)
         indent-increment (get env :param/indent-increment 2)]
     (cond
-      (vector? value)
-      (let [prefix (get env :param/prefix "")
+      #_ (vector? value)
+      #_ (let [prefix (get env :param/prefix "")
             first-prefix (string/join [prefix "- "])]
         (first
           (reduce
@@ -21,26 +25,24 @@
             value)))
       (map? value)
       (let [prefix (get env :param/prefix "")
-            unprefix ""]
+            length (count prefix)
+            unprefix (blanks length)]
         (first
           (reduce
             (fn [[lst prefix] [k v]]
               (let [env (into env {:param/prefix prefix})
-                    kv (first (asString- (into env {:param/value k})))
+                    kv (first (asString- (into env {:param/prefix prefix
+                                                    :param/value k})))
                     kpv (string/join [kv ":"])
                     lst (conj lst kpv)
-                    lst (into lst (asString- (into env {:param/value          v
-                                                        :param/initial-indent (+ initial-indent indent-increment)})))]
+                    lst (into lst (asString- (into env {:param/prefix (string/join unprefix "  ")
+                                                        :param/value          v})))]
                 [lst unprefix]))
             [[] prefix]
             value)))
       :else
-      (let [initial-indent (get env :param/initial-indent 0)
-            prefix
-            (if (= initial-indent 0)
-              ""
-              (string/join (repeat initial-indent " ")))
-            s (pr-str value)
+      (let [prefix (get env :param/prefix "")
+            s (str value)
             j (string/join [prefix s])]
         [j]))))
 
